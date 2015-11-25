4d41e9201e9da28665689547f1f58765f686ff5f
Greg Look <greg@greg-look.net>
2015-11-24 20:18:30 -0800

    Set release version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/multicodec "0.4.0-SNAPSHOT"
+(defproject mvxcvi/multicodec "0.4.0"
   :description "Clojure implementation of the multicodec standard."
   :url "https://github.com/greglook/clj-multicodec"
   :license {:name "Public Domain"
@@ -14,6 +14,6 @@
 
   :codox
   {:metadata {:doc/format :markdown}
-   :source-uri "https://github.com/greglook/clj-multicodec/blob/develop/{filepath}#L{line}"
+   :source-uri "https://github.com/greglook/clj-multicodec/blob/0.4.0/{filepath}#L{line}"
    :doc-paths [""]
    :output-path "doc/api"})
