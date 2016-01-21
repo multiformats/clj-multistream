7bdd9902634e454c9c932dff1f0159a23a567685
Greg Look <greg@greg-look.net>
2016-01-20 17:27:19 -0800

    Add filter codec and tests.

---
+++ src/multicodec/codecs/filter.clj
@@ -0,0 +1,50 @@
+(ns multicodec.codecs.filter
+  "Logical codec which wraps another codec to transform the data before it is
+  encoded or after it is decoded."
+  (:require
+    [multicodec.core :as codec]))
+
+
+;; ## Filter Codec
+
+(defrecord FilterCodec
+  [header subcodec encoding-fn decoding-fn]
+
+  codec/Encoder
+
+  (encode!
+    [this output value]
+    (codec/encode!
+      subcodec
+      output
+      (if encoding-fn
+        (encoding-fn value)
+        value)))
+
+
+  codec/Decoder
+
+  (decode!
+    [this input]
+    (let [value (codec/decode! subcodec input)]
+      (if decoding-fn
+        (decoding-fn value)
+        value))))
+
+
+(defn filter-codec
+  "Creates a new filter codec, wrapping the given subcodec. Opts may include:
+
+  - `:encoding` function which will transform values before they are encoded
+  - `:decoding` function which will transform values after they are decoded"
+  [subcodec & {:as opts}]
+  (FilterCodec.
+    (:header subcodec)
+    subcodec
+    (:encoding opts)
+    (:decoding opts)))
+
+
+;; Remove automatic constructor functions.
+(ns-unmap *ns* '->FilterCodec)
+(ns-unmap *ns* 'map->FilterCodec)
---
+++ test/multicodec/codecs/filter_test.clj
@@ -0,0 +1,25 @@
+(ns multicodec.codecs.filter-test
+  (:require
+    [clojure.test :refer :all]
+    [multicodec.core :as codec]
+    [multicodec.codecs.filter :as filter]
+    [multicodec.codecs.test-utils :refer [mock-codec]]))
+
+
+(deftest filter-codec
+  (testing "without coding filters"
+    (let [fltr (filter/filter-codec (mock-codec :foo "/foo"))]
+      (testing "encoding roundtrip"
+        (let [encoded (codec/encode fltr 1234)]
+          (is (= 4 (count encoded)) "should write the correct number of bytes")
+          (is (= [:foo "1234"]
+                 (codec/decode fltr encoded)))))))
+  (testing "with coding filters"
+    (let [fltr (filter/filter-codec (mock-codec :foo "/foo")
+                                    :encoding #(vector :encoded %)
+                                    :decoding #(vector :decoded %))]
+      (testing "encoding roundtrip"
+        (let [encoded (codec/encode fltr 1234)]
+          (is (= 15 (count encoded)) "should write the correct number of bytes")
+          (is (= [:decoded [:foo "[:encoded 1234]"]]
+                 (codec/decode fltr encoded))))))))
