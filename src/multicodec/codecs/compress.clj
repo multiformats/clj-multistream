250d6a17094635b957bc2ca327cb4909ced8b3e3
Greg Look <greg@greg-look.net>
2018-01-06 20:24:55 -0800

    Update compression codec ns.

--- src/multicodec/codecs/compress.clj
+++ src/multicodec/codecs/compress.clj
@@ -12,57 +12,30 @@
       GZIPOutputStream)))
 
 
-(defrecord CompressionCodec
-  [header codec wrap-input wrap-output]
+;; ## GZIP
 
-  codec/Encoder
+(defcodec GZIPCodec
+  [header]
 
-  (encodable?
-    [this value]
-    (codec/encodable? codec value))
-
-
-  (encode!
-    [this output value]
-    (codec/encode! codec (wrap-output output) value))
+  (processable?
+    [this hdr]
+    (= hdr header))
 
 
-  codec/Decoder
-
-  (decodable?
-    [this header']
-    (codec/decodable? codec header'))
-
-
-  (decode!
-    [this input]
-    (codec/decode! codec (wrap-input input))))
+  (encode-stream
+    [this _ stream]
+    ; TODO: assert stream is an OutputStream
+    (codec/write-header! stream header)
+    (GZIPOutputStream. ^OutputStream stream))
 
 
-(alter-meta! #'->CompressionCodec assoc :private true)
-(alter-meta! #'map->CompressionCodec assoc :private true)
-
-
-(defn compress-codec
-  "Creates a new compression codec, wrapping the given codec. The wrapper
-  functions are called on the input and output streams passed to the next
-  codec.
-
-  - `:header` string which overrides inheriting the codec's header
-  - `:input` function which will transform bytes after they are encoded
-  - `:output` function which will transform bytes before they are decoded"
-  [codec & {:as opts}]
-  (->FilterCodec
-    (:header opts)
-    codec
-    (:input opts)
-    (:output opts)))
+  (decode-stream
+    [this _ stream]
+    ; TODO: assert stream is an InputStream
+    (GZIPInputStream. ^InputStream stream)))
 
 
 (defn gzip-codec
   "Creates a compression codec which will apply GZIP to the encoded data."
   [codec]
-  (compress-codec codec
-    :header "/gzip"
-    :input #(GZIPInputStream. ^InputStream %)
-    :output #(GZIPOutputStream. ^OutputStream %)))
+  (->GZIPCodec "/gzip/"))
